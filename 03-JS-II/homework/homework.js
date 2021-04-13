62249054e44431ce9ead86e2f85368b90c318a98
davidmenlop <davidmenlop@gmail.com>
2021-04-12 21:54:37 -0500

    avances homework JS II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -48,6 +48,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma ==='aleman'){
+    return "Guten Tag!";
+  } else if (idioma === 'mandarin'){
+    return "Ni Hao!";
+  } else if (idioma === 'ingles'){
+    return "Hello!"
+  } else{
+    return "Hola!"
+  }
 }
 
 function colors(color) {
@@ -58,18 +67,41 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";    
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+if (numero<50 && numero>20){
+  return true;
+} else{
+  return false;
+}
+
 }
 
 function esEntero(numero) {
@@ -80,6 +112,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero - Math.floor(numero) == 0) {
+    return true;
+} else {
+    return false;
+}
 }
 
 function fizzBuzz(numero) {
